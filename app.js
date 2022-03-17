473c737fb6bcf8c4a872b2bcb0c34496a4866f8a
Steven <svramirez20@gmail.com>
2022-03-17 01:35:40 -0400

    movement added

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ const hitStart = document.querySelector('#start-button');
 const hitPause = document.querySelector('#pause-button');
 const wholeGrid = document.querySelectorAll('.grid div');
 let currentBox = 76;
+const gridWidth = 9;
 
-function move() {
-    console.log(wholeGrid)
-    console.log(wholeGrid[currentBox])
+function move(event) {
+    //console.log(event)
+    wholeGrid[currentBox].classList.remove('frog')
+    switch(event.key) {
+        case "ArrowLeft" :
+            currentBox -= 1
+            break
+        case "ArrowRight" :
+            currentBox += 1
+            break
+        case "ArrowUp" :
+            currentBox -= gridWidth
+            break
+        case "ArrowDown" :
+            currentBox += gridWidth
+            break
+    }
+
     wholeGrid[currentBox].classList.add('frog')
 }
 
